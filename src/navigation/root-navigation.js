2c6ae5302c5d8e588dec8c0844482128e9308c55
Dat Huynh <huynhtuandat2997@gmail.com>
2021-11-22 21:42:16 +0700

    Create HomeScreen

--- src/navigation/root-navigation.js
+++ src/navigation/root-navigation.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen, DetailScreen} from '../screens';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  HomeScreen,
+  DetailScreen,
+  StreamScreen,
+  ProfileScreen,
+} from '../screens';
+import {screenName} from '../utils/constant';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const RootNavigation = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Detail" component={DetailScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen name={screenName.home} component={HomeScreen} />
+      <Tab.Screen name={screenName.stream} component={StreamScreen} />
+      <Tab.Screen name={screenName.profile} component={ProfileScreen} />
+    </Tab.Navigator>
   );
 };
 
